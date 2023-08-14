7bc716667c9444af3d4c0840067073115def173c
Eugene and monty <eugene.and.monty@gmail.com>
2023-08-13 20:47:52 -0500

    refactoring

--- ai_backend_ios_andr/settings.py
+++ ai_backend_ios_andr/settings.py
@@ -4,26 +4,18 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'fake-backend.chickenkiller.com', '143.244.154.153', 'airesearchfund.com']
+ALLOWED_HOSTS = [
+    '0.0.0.0', 
+    '127.0.0.1', 
+    '143.244.154.153', 
+    'airesearchfund.com'
+    ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-
-# DEBUG = False
-# SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True
-# SECURE_SSL_REDIRECT = True
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -68,9 +60,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ai_backend_ios_andr.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -84,9 +73,6 @@ DATABASES = {
         }
     }
 }
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -103,9 +89,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# Internationalization
-# https://docs.djangoproject.com/en/4.1/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -114,12 +97,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-python3 manage.py makemigrations
-python3 manage.py migrate
-gunicorn ai_backend_ios_andr.wsgi:application --bind 0.0.0.0:8000 --workers 3 -k "gevent"
-gunicorn --limit-request-line 0
